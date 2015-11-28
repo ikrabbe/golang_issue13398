9275265d2fa094e7553ea93e2d5367b2e9673306
Ingo Krabbe <ik@protel-net.com>
2015-11-28 13:38:08 +0100

    test with a proxy structure

--- issue13398-buffer-race.go
+++ issue13398-buffer-race.go
@@ -1,16 +1,33 @@
 package main
 
 import (
-	"log"
 	"bytes"
 	"fmt"
+	"io"
+	"log"
 	"os"
 	"os/exec"
 	"time"
 )
 
+type MyBuffer struct {
+	intbuf bytes.Buffer
+}
+
+func (mb *MyBuffer) WriteTo(w io.Writer) (n int64, err error) {
+	return mb.intbuf.WriteTo(w)
+}
+
+func (mb *MyBuffer) Len() int {
+	return mb.intbuf.Len()
+}
+
+func (mb *MyBuffer) Write(p []byte) (n int, err error) {
+	return mb.intbuf.Write(p)
+}
+
 func main() {
-	out := bytes.Buffer{}
+	out := MyBuffer{}
 	cmd := exec.Command("cmd/test_command")
 	cmd.Stdout = &out
 	err := cmd.Start()
